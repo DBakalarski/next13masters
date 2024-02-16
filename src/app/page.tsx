3221808e155615d161feb9dbb0141ab3cbbb0c26
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2024-02-16 21:07:44 +0100

    Update layout and remove unnecessary section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,9 +45,5 @@ const products: ProductItemType[] = [
 ];
 
 export default function Home() {
-	return (
-		<section className="mx-auto max-w-md p-12 sm:max-w-2xl ">
-			<ProductList products={products} />
-		</section>
-	);
+	return <ProductList products={products} />;
 }
