6fa2411c726987ab3ceddcd168fb3d2348cb3dcb
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2024-02-19 21:30:42 +0100

    Add generateStaticParams function to fetch and return product IDs

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -4,6 +4,13 @@ import { ProductCoverImage } from "@/ui/atoms/ProductCoverImg";
 import { ProductListItemDescription } from "@/ui/atoms/ProductListItemDescription";
 import { SuggestedProducts } from "@/ui/organisms/SuggestedProducts";
 
+export async function generateStaticParams() {
+	const res = await fetch(`https://naszsklep-api.vercel.app/api/products`);
+	const products = (await res.json()) as { id: string; title: string }[];
+
+	return products.map((product) => ({ productId: product.id })).slice(0, 3);
+}
+
 export default async function SingleProduct({ params }: { params: { productId: string } }) {
 	const product = await geProductResponseItemTypeById(params.productId);
 	return (
