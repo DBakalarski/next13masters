d717225de2ca500c75c83feaa626fcdca79ac4a0
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2024-02-21 21:47:40 +0100

    Add Tailwind Typography plugin and create MDX page

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
+import remarkGfm from "remark-gfm";
+import createMDX from "@next/mdx";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	// Configure `pageExtensions`` to include MDX files
+	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 	experimental: {
 		typedRoutes: true,
+		mdxRs: true,
 	},
+	// Optionally, add any other Next.js config below
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+	// Add markdown plugins here, as desired
+	options: {
+		remarkPlugins: [remarkGfm],
+		rehypePlugins: [],
+	},
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
--- src/app/(static-pages)/polityka-prywatnosci/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Policy() {
-	return (
-		<>
-			<h1 className="text-4xl font-bold">Polityka prywatnosci</h1>
-			<p className="text-xl">lorem ipsum </p>
-		</>
-	);
-}
