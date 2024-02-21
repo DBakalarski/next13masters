a44c167451f0d230ca7626712e1d791c654f27d3
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2024-02-21 11:00:21 +0100

    Refactor ActiveLink component to accept generic route type

--- src/ui/atoms/ActiveLink.tsx
+++ src/ui/atoms/ActiveLink.tsx
@@ -3,21 +3,21 @@
 import Link from "next/link";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
-import { type ReactNode, useMemo } from "react";
+import { type ReactNode } from "react";
+import { type Route } from "next";
 
-export const ActiveLink = ({ href, children }: { href: string; children: ReactNode }) => {
+export const ActiveLink = <T extends string>({
+	href,
+	children,
+}: {
+	href: Route<T> | URL;
+	children: ReactNode;
+}) => {
 	const pathname = usePathname();
 	const isActive = pathname === href;
 
-	const urlObject = useMemo(() => {
-		if (typeof href === "string") {
-			return { pathname: href };
-		}
-		return href;
-	}, [href]);
-
 	return (
-		<Link className={clsx(`hover:text-red-600`, isActive && `underline`)} href={urlObject}>
+		<Link className={clsx(`hover:text-red-600`, isActive && `underline`)} href={href}>
 			{children}
 		</Link>
 	);
